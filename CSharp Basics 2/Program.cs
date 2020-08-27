ed1529d45b6bb8885db8168720cd88531e65a1a4
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-08-27 11:25:16 -0400

    This is showing the thingys

--- CSharp Basics 2/Program.cs
+++ CSharp Basics 2/Program.cs
@@ -8,15 +8,16 @@ namespace CSharpBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Tristian!");
-            MyString();
-
-            Console.ReadLine();
-
+            DataTypes();
         }
 
         static void MyString()
         {
+
+            // This is a comment
+            /// 1
+            /// 2
+            /// 3
             Console.Write("what year were you born?");
             int yearBorn = Convert.ToInt32(Console.ReadLine());
             Console.Write("What is your age?");
@@ -29,8 +30,32 @@ namespace CSharpBasics
             string theLie = "I am not a ninja.";
             Console.WriteLine("This is my String. " + myString1);
             Console.WriteLine($"Here are two truths and a lie. {theTruth}, {theLie}");
-             Console.WriteLine("Mr. Sekol is the best ever.");
+            Console.WriteLine("Mr. Sekol is the best ever.");
 
         }
+
+        static void DataTypes()
+        {
+            int myAge = 99;
+            string name = "Bob The Builder";
+            bool awesome = true;
+            char grade = 'F'; // Chars are single characters.  requires single quote. 'F'
+            float altitude = 90.265141f; //float requires 'f' at the end.
+            double bankBalance = -40000000000;
+            decimal pi = 3.14159265359M; // decimal requires an M at the end.
+
+
+            /// OutPut
+            /// 
+            Console.WriteLine($"myAge = {myAge}");
+            Console.WriteLine($"string = {name}");
+            Console.WriteLine($"bool = {awesome}");
+            Console.WriteLine($"char = {grade}");
+            Console.WriteLine($"float = {altitude}");
+            Console.WriteLine($"double = {bankBalance}");
+            Console.WriteLine($"decimal = {pi}");
+           
+
+      }
     }
 }
